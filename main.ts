e742150a13fd27f7ed077fcf766d8f5bc2b38fef
papaya123456 <147636826+papaya123456@users.noreply.github.com>
2024-09-06 18:08:58 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,134 +1,120 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
+// Define the main plugin class
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+	// Called when the plugin is loaded
 	async onload() {
-		await this.loadSettings();
+		// Convert units in the active file when the plugin is loaded
+		this.readActiveFileAndConvertUnits();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		// Set up an event listener for editor changes
+		this.app.workspace.on("editor-change", (editor) => {
+			const content = editor.getDoc().getValue();
+			this.convertUnitsInContent(content);
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// Set up an event listener for active leaf changes
+		this.app.workspace.on("active-leaf-change", () => {
+			this.readActiveFileAndConvertUnits();
+		});
+	}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+	// Read the active file and convert units
+	async readActiveFileAndConvertUnits() {
+		const file = this.app.workspace.getActiveFile();
+		if (file) {
+			const content = await this.app.vault.read(file);
+			this.convertUnitsInContent(content);
+		}
+	}
+
+	// Convert units in the provided content and update the file
+	async convertUnitsInContent(content: string) {
+		const updatedContent = this.convertToMetric(content);
+		const file = this.app.workspace.getActiveFile();
+		if (file) {
+			await this.app.vault.modify(file, updatedContent);
+		}
+	}
+
+	// Convert imperial units in the content to metric units
+	convertToMetric(content: string): string {
+		const intOrFloat = '([0-9]+([,\\.][0-9]+)?)';
+		const unitSuffix = '([^a-zA-Z]|$)';
+
+		// Define the units to convert and their conversion multipliers
+		const toConvert = [
+			{ regex: new RegExp('(' + intOrFloat + ' ?mi(les?)?)' + unitSuffix, 'ig'), unit: 'km', multiplier: 1.60934 },
+			{ regex: new RegExp('(' + intOrFloat + ' ?f(ee|oo)?t)' + unitSuffix, 'ig'), unit: 'm', multiplier: 0.3048 },
+			{ regex: new RegExp('(' + intOrFloat + ' ?yards)' + unitSuffix, 'ig'), unit: 'm', multiplier: 0.9144 },
+			{ regex: new RegExp('(' + intOrFloat + ' ?(pound|lb)s?)' + unitSuffix, 'ig'), unit: 'kg', multiplier: 0.453592 },
+			{ regex: new RegExp('(' + intOrFloat + ' ?gallons?)' + unitSuffix, 'ig'), unit: 'L', multiplier: 3.78541 },
+			{ regex: new RegExp('(' + intOrFloat + ' ?stones?)' + unitSuffix, 'ig'), unit: 'kg', multiplier: 6.35029 },
+			{ regex: new RegExp('(' + intOrFloat + ' ?inch(es)?)' + unitSuffix, 'ig'), unit: 'cm', multiplier: 2.54 },
+		];
+
+		// Convert the original amount using the provided multiplier
+		function convert(originalAmount: string, multiplier: number) {
+			const amount = parseFloat(originalAmount);
+			let convertedAmount = amount * multiplier;
+			convertedAmount = Math.round(convertedAmount * 100) / 100;
+			return convertedAmount;
+		}
+
+		// Generate the converted string for output
+		function convertForOutput(originalAmount: string, unitIndex: number) {
+			const multiplier = toConvert[unitIndex].multiplier;
+			const unit = toConvert[unitIndex].unit;
+			originalAmount = originalAmount.replace(/,/g, '');
+			const convertedAmount = convert(originalAmount, multiplier);
+			const convertedString = ` (${convertedAmount} ${unit})`;
+			return convertedString;
+		}
+
+		function convertSimpleUnits(text: string) {
+			const len = toConvert.length;
+			for (let i = 0; i < len; i++) {
+			    if (text.search(toConvert[i].regex) !== -1) {
+					let matches;
+					while ((matches = toConvert[i].regex.exec(text)) !== null) {
+					const fullMatch = matches[1];
+					const originalAmount = matches[2];
+					const convertedString = convertForOutput(originalAmount, i);
+					const insertIndex = matches.index + fullMatch.length;
+					
+					// Check if the conversion has already happened directly after the text
+					const followingText = text.substring(insertIndex, insertIndex + convertedString.length);
+					if (followingText !== convertedString) {
+						text = insertAt(text, convertedString, insertIndex);
+						}
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
 			}
-		});
+			return text;
+		}
+		
+		function insertAt(text: string, insert: string, index: number) {
+			return text.slice(0, index - 1) + insert + text.slice(index);
+		}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// Convert feet and inches to meters and append the converted value to the text
+		function feetAndInchesToMeter(text: string) {
+			const matches = text.match(/([0-9]{0,3})'([0-9][0-2]?)"/);
+			if (matches) {
+				const original = matches[0];
+				const feet: number = parseInt(matches[1]);
+				const inches: number = parseInt(matches[2]);
+				let meter = (feet * 0.3048) + (inches * 0.0254);
+				meter = Math.round(meter * 100) / 100;
+				meter = parseFloat(meter.toFixed(2));
+				text = text.replace(new RegExp('(' + original + ')'), '$1 (' + meter + ')');
+			}
+			return text;
+		}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		// Convert simple units and feet/inches to metric in the content
+		content = convertSimpleUnits(content);
+		content = feetAndInchesToMeter(content);
+		return content;
 	}
 }
