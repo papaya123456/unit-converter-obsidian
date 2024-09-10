d886dcea4a5a40795b28edac9a3a773d7f202286
papaya123456 <147636826+papaya123456@users.noreply.github.com>
2024-09-10 17:16:53 +0200

    Refactor unit conversion logic and remove unused code

--- main.ts
+++ main.ts
@@ -18,7 +18,6 @@ export default class MyPlugin extends Plugin {
 	// Main processing function to handle unit conversion
 	processContent(content: string): string {
 		const words = this.splitTextBySpace(content);
-		//this.printWordsWithIndices(words);
 
 		this.convertWords(words);
 
@@ -64,52 +63,5 @@ export default class MyPlugin extends Plugin {
 
 		}
 	}
-	
-	/*
-	// *unction to print words with their indices
-	printWordsWithIndices(words: string[]) {
-		words.forEach((word, index) => {
-			console.log(`Index: ${index}, Word: "${word}"`);
-		});
-	}
-	*/
-	/*
-	// Function to convert units if not already converted
-	convertUnitsIfNecessary(content: string): string {
-		let updatedContent = content;
-	
-		// Iterate over each conversion pattern
-		unitsCheckForConversion.forEach(({ regex, unit: conversionFunction }) => {
-			const matches = this.findMatches(content, regex);
-	
-			if (matches) {
-				matches.forEach(match => {
-					if (!this.unitAlreadyConverted(content, match)) {
-						const convertedValue = typeof conversionFunction === 'function' ? (conversionFunction as (match: string) => string)(match) : '';
-						updatedContent = this.insertConvertedValue(updatedContent, match, convertedValue);
-						console.log(`Converted "${match}" to "${convertedValue}"`);
-					}
-				});
-			}
-		});
-	
-		return updatedContent;
-	}
 
-	// Function to find matches based on regex
-	findMatches(content: string, regex: RegExp): string[] | null {
-		return content.match(regex);
-	}
-
-	// Function to check if the unit has already been converted
-	unitAlreadyConverted(content: string, match: string): boolean {
-		const regex = new RegExp(`${match}\\s*\\(\\s*\\d+(\\.\\d+)?\\s*\\)`);
-		return regex.test(content);
-	}
-
-	// Function to insert the converted value into the content
-	insertConvertedValue(content: string, match: string, convertedValue: string): string {
-		return content.replace(match, `${match} (${convertedValue})`);
-	}
-		*/
 }
